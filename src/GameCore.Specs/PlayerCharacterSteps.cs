1f95abfbdb9e9b84b2cb909b42806ec2f9036417
Andrew Withers <andreww@thomasdev.co.uk>
2018-10-16 16:20:14 +0100

    Extend PlayerCharacter definition, and refactor tests to use paramaterised step methods

--- src/GameCore.Specs/PlayerCharacterSteps.cs
+++ src/GameCore.Specs/PlayerCharacterSteps.cs
@@ -13,35 +13,17 @@ namespace GameCore.Specs
         {
             _player = new PlayerCharacter();
         }
+
+        [When(@"I take (.*) damage")]
+        public void WhenITakeDamage(int damage)
+        {
+            _player.Hit(damage);
+        }
         
-        [When(@"I take 0 damage")]
-        public void WhenITake0Damage()
+        [Then(@"My health should be (.*)")]
+        public void ThenMyHealthShouldBe(int expectedHealth)
         {
-            _player.Hit(0);
-        }
-        
-        [Then(@"My health should be 100")]
-        public void ThenMyHealthShouldBe100()
-        {
-            Assert.AreEqual(100, _player.Health);
-        }
-
-        [When(@"I take 40 damage")]
-        public void WhenITake40Damage()
-        {
-            _player.Hit(40);
-        }
-
-        [Then(@"My health should be 60")]
-        public void ThenMyHealthShouldBe60()
-        {
-            Assert.AreEqual(60, _player.Health);
-        }
-
-        [When(@"I take 100 damage")]
-        public void WhenITake100Damage()
-        {
-            _player.Hit(100);
+            Assert.AreEqual(expectedHealth, _player.Health);
         }
 
         [Then(@"I should be dead")]
